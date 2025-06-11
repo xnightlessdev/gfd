a747f4b84f63638c1db4fbd244973662510d62cb
xnightlessdev <shadowiscool@zombi3slayz.com>
2025-06-11 18:59:39 -0500

    Update themes.js

--- public/js/themes.js
+++ public/js/themes.js
@@ -1,26 +1,5 @@
-function loadTheme(theme = localStorage.getItem("theme") || "") {
-  document.documentElement.className = theme;
+function loadTheme(theme = localStorage.getItem("theme")) {
+    document.documentElement.className = theme;
 }
 
-function saveTheme(theme) {
-  localStorage.setItem("theme", theme);
-  loadTheme(theme);
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const themeSelect = document.getElementById("theme-select");
-  const applyBtn = document.getElementById("applyThemeBtn");
-
-  loadTheme();
-
-  if (themeSelect) {
-    themeSelect.value = localStorage.getItem("theme") || "";
-  }
-
-  if (applyBtn && themeSelect) {
-    applyBtn.addEventListener("click", () => {
-      const selectedTheme = themeSelect.value;
-      saveTheme(selectedTheme);
-    });
-  }
-});
+loadTheme();
