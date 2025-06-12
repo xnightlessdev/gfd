6c2099513b54f5aca4b12f738dc9d32f94f73959
xnightlessdev <shadowiscool@zombi3slayz.com>
2025-06-11 19:08:09 -0500

    Update themes.js

--- public/js/themes.js
+++ public/js/themes.js
@@ -1,5 +1,15 @@
+
 function loadTheme(theme = localStorage.getItem("theme")) {
+  if (theme) {
     document.documentElement.className = theme;
+  } else {
+    document.documentElement.className = ""; 
+  }
+}
+
+function saveTheme(theme) {
+  localStorage.setItem("theme", theme);
+  document.documentElement.className = theme;
 }
 
 loadTheme();
