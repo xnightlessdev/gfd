56c23974fb4f0e244e2f1d9baee83c021b78d29b
xnightlessdev <shadowiscool@zombi3slayz.com>
2025-06-12 09:38:02 -0500

    Update themes.js

--- public/js/themes.js
+++ public/js/themes.js
@@ -1,13 +1,9 @@
 function loadTheme(theme = localStorage.getItem("theme") || "default") {
     document.documentElement.className = "";
     document.documentElement.classList.add(theme);
+    document.documentElement.style.display = "none";
+    document.documentElement.offsetHeight;
+    document.documentElement.style.display = "";
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    loadTheme();
-    window.addEventListener("storage", (event) => {
-        if (event.key === "theme") {
-            loadTheme(event.newValue || "default");
-        }
-    });
-});
+document.addEventListener
